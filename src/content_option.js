7efd93f1d019d5ae2819222d15f76e6d5f7bfeec
unknown <deoralaxminarayan@gmail.com>
2024-03-26 14:20:29 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+import myImg from "./assets/images/vinay2.jpeg"
+const logotext = "VINAY DEORA";
+const meta = {
+    title: "VINAY DEORA  | React Developer",
+    description: "I am Vinay Deora, a proficient ReactJS developer dedicated to creating dynamic and user-centric web applications. My expertise in front-end development empowers me to deliver innovative and efficient solutions for every project, consistently exceeding client expectations. Let's collaborate to bring your digital visions to life.",
+};
+
+const introdata = {
+    title: "I’m Vinay  DeOra",
+    animated: {
+        first: "React Js Developer",
+        second: "I code cool websites",
+        third: "I Love code",
+    },
+    description: "I am Vinay Deora, a proficient ReactJS developer dedicated to creating dynamic and user-centric web applications. My expertise in front-end development empowers me to deliver innovative and efficient solutions for every project, consistently exceeding client expectations. Let's collaborate to bring your digital visions to life.",
+    your_img_url:myImg,
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "I'm Vinay Deora, a 2022 MCA graduate with a passion for frontend development. Currently, I'm channeling my skills into creating captivating web experiences as a ReactJS developer. Let's build something extraordinary together!",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "ReactJs",
+        value:80,
+    },
+    {
+        name: "Javascript",
+        value: 70,
+    },
+    {
+        name: "Redux",
+        value: 80,
+    },
+    {
+        name: "Tailwind css",
+        value: 60,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "Firebase",
+        value: 40,
+    },
+];
+
+const services = [{
+        title: "Website Development",
+        description: "Transforming your ideas into stunning, responsive websites that engage and captivate users. Leveraging the power of React to create seamless, dynamic, and interactive user interfaces.",
+    },
+    {
+        title: "User Engagement",
+        description: "Crafting websites that actively engage and captivate your audience.",
+    },
+    {
+        title: "ReactJS Expertise",
+        description: "Proficiently utilizing React to design fluid, dynamic, and engaging user interfaces.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://th.bing.com/th/id/OIP.oOztoL7_nfMoFHMSKbKuNAHaFj?w=251&h=188&c=7&r=0&o=5&pid=1.7",
+        description: "FOOD-ORDRING_APP",
+        link: "https://64e393ff269d06034778d4eb--stirring-kangaroo-7f26c2.netlify.app/",
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.vhV_MaMlW5td5UVbmtcjcAHaEo?w=292&h=182&c=7&r=0&o=5&pid=1.7",
+        description: "NETFLIX-UI",
+        link: "https://vinaydeora-netflix-clone-react.netlify.app/",
+    },
+    {
+        img: "https://th.bing.com/th/id/OIP.Il817xj1wRi_1QaN0UsNQQHaFj?w=247&h=185&c=7&r=0&o=5&pid=1.7",
+        description: "NETFLIX-GPT PART-1 ",
+        link: "https://65111abad1e26851dedbf789--kaleidoscopic-chaja-0615fd.netlify.app/",
+    },
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+
+    // {
+    //     img: "https://picsum.photos/400/600/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/300/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/550/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+    // {
+    //     img: "https://picsum.photos/400/700/?grayscale",
+    //     description: "The wisdom of life consists in the elimination of non-essentials.",
+    //     link: "#",
+    // },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vinaydeora99@gmail.com",
+    YOUR_FONE: "6367609049",
+    description: "I am Vinay Deora, a proficient ReactJS developer dedicated to creating dynamic and user-centric web applications. My expertise in front-end development empowers me to deliver innovative and efficient solutions for every project, consistently exceeding client expectations. Let's collaborate to bring your digital visions to life.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/vinay-deora",
+    Instagram: "https://instagram.com/__vinay_deora__?igshid=OGQ5ZDc2ODk2ZA==",
+    linkedin: "https://www.linkedin.com/in/vinay-deora-b713ab233/",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
